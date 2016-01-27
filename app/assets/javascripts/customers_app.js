e7e955bfdaf1193ed5b3e10dd3eaf2cfa1b8bb8c
rhuberdeau <rhuberdeau@gmail.com>
2016-01-27 13:41:27 -0500

    Adding angular routes, modifying the search controller and adding a details controller

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,8 +1,28 @@
-var app = angular.module('customers',[]);
+var app = angular.module(
+  'customers',
+  [
+    'ngRoute',
+    'templates'
+  ]
+);
+
+app.config([
+  "$routeProvider",
+  function($routeProvider){
+
+    $routeProvider.when("/",{
+      controller:"CustomerSearchController",
+      templateUrl:"customer_search.html"
+    }).when("/:id", {
+      controller:"CustomerDetailController",
+      templateUrl:"customer_detail.html"
+    });
+  }
+]);
 
 app.controller("CustomerSearchController", [
-  "$scope","$http",
-  function($scope,$http) {
+  "$scope","$http","$location",
+  function($scope,$http,$location) {
     var page = 0;
     $scope.customers = []
 
@@ -30,5 +50,27 @@ app.controller("CustomerSearchController", [
       page = page + 1;
       $scope.search($scope.keywords);
     }
+
+    $scope.viewDetails = function(customer) {
+      $location.path("/" + customer.id);
+    }
+  }
+]);
+
+app.controller("CustomerDetailController", [
+          "$scope","$http","$routeParams",
+  function($scope, $http, $routeParams) {
+
+    var customerId = $routeParams.id;
+    $scope.customer = {};
+
+    $http.get(
+      "/customers/" + customerId + ".json"
+    ).then(function(response) {
+        $scope.customer = response.data;
+      },function(response){
+        alert("There was a problem:" + response.status);
+      }
+    );
   }
 ]);
